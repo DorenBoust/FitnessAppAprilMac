269c557e8f4efc1146a1c869856d95b90796f547
lior Dorenboust <liordorenboust@gmail.com>
2020-04-10 22:15:04 +0300

    Add tabs to Articals Fragment

--- app/src/main/java/com/example/fitnessapp/main/ArticlesFragment.java
+++ app/src/main/java/com/example/fitnessapp/main/ArticlesFragment.java
@@ -7,19 +7,35 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.fitnessapp.R;
+import com.example.fitnessapp.articals.ArticalsFragmentTab;
+import com.example.fitnessapp.articals.PageAdapter;
+import com.example.fitnessapp.articals.RecipesFragmentTab;
 import com.example.fitnessapp.keys.KeysBundle;
 import com.example.fitnessapp.user.User;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class ArticlesFragment extends Fragment {
 
     private ArticlesViewModel mViewModel;
     private User user;
+
+    //tab
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private PageAdapter pageAdapter;
+    private TabItem tabRecipes;
+    private TabItem tabArticals;
+
+    private RecipesFragmentTab recipesFragmentTab;
+    private ArticalsFragmentTab articalsFragmentTab;
 
     public static ArticlesFragment newInstance() {
         return new ArticlesFragment();
@@ -32,6 +48,29 @@ public class ArticlesFragment extends Fragment {
 
         user = (User) getArguments().getSerializable(KeysBundle.USER_DATA_TO_FRAGMENT);
         System.out.println("Articals" + user);
+
+
+        tabLayout = v.findViewById(R.id.layoutTab);
+        tabRecipes = v.findViewById(R.id.tabRecipes);
+        tabArticals = v.findViewById(R.id.tabArticals);
+        viewPager = v.findViewById(R.id.viewPager);
+        tabLayout = v.findViewById(R.id.layoutTab);
+
+        recipesFragmentTab = new RecipesFragmentTab();
+        articalsFragmentTab = new ArticalsFragmentTab();
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        PageAdapter pageAdapter = new PageAdapter(getParentFragmentManager(), 0);
+        pageAdapter.addFragment(recipesFragmentTab, "מתכונים");
+        pageAdapter.addFragment(articalsFragmentTab, "מאמרים");
+        viewPager.setAdapter(pageAdapter);
+
+
+//        pageAdapter = new PageAdapter(getChildFragmentManager(), tabLayout.getTabCount());
+//        viewPager.setAdapter(pageAdapter);
+
+
         return v;
     }
 
